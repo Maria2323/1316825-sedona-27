0b8dfd8bda66db4a33d415846f8fa4b93368273d
Maria2323 <60069591+Maria2323@users.noreply.github.com>
2020-03-09 19:59:06 +0300

    Все готово кроме прозрачности футера в разных браузерах

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+var button = document.querySelector('.search');
+var form = document.querySelector('.form');
+var clickHandler = function() {
+  form.classList.toggle('form-hidden');
+};
+button.addEventListener('click', clickHandler);
